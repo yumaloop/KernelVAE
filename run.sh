3d0900f4461a43452b5497bcf23b72a983c074f1
yumaloop <yumaloop@gmail.com>
2019-10-21 08:16:06 +0900

    rewrite docs

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # python3 run_experiments.py \
 python3 run_vae.py \
---num_train=5000 \
---num_eval=1000 \
+--num_train=500 \
+--num_eval=100 \
 --hparams='nonlinearity=relu,depth=100,weight_var=1.79,bias_var=0.83' \
