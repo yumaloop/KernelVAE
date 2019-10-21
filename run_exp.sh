bb370c707056944434bafd282bf190366f9de6fe
yumaloop <yumaloop@gmail.com>
2019-10-21 18:51:23 +0900

    update repo 2019年 10月 21日 月曜日 18:51:23 JST

---
+++ run_exp.sh
@@ -0,0 +1,8 @@
+python3 run_experiments.py \
+--num_train=500 \
+--num_eval=100 \
+--hparams='nonlinearity=relu,depth=100,weight_var=1.79,bias_var=0.83' \
+--n_gauss=501 \
+--n_var=501 \
+--n_corr=500 \
+--max_gauss=10
